ea3408b2a48eb2231451dd00c27524c11e30759f
Bigazy <bigazyaudan@gmail.com>
2025-02-04 13:23:33 +0500

    Added Cw_3 folder with book API

---
+++ cw_3/book/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
